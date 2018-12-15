56befcf0330a6623f2477f9604f95e7a98a3a339
NSLog0 <prachaya.su@gmail.com>
2018-12-16 00:02:35 +0700

    move front-end

--- ui/src/helpers/cookie.js
+++
@@ -1,6 +0,0 @@
-const LOGIN_KEY = '_smartward_login_pincode_status';
-const OFFICER_KEY = '_smartward_user_token';
-const PATIEN_KEY = '_smartward_patien_id';
-const EXPIRES_TIME = 7;
-
-export { LOGIN_KEY, OFFICER_KEY, PATIEN_KEY, EXPIRES_TIME };
--- ui/src/helpers/http.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-let root = 'http://localhost:5000';
-
-if (process.env.NODE_ENV === 'production') {
-  root = 'http://localhost:8000';
-}
-
-export {};
