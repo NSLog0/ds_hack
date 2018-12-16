0b56f21a5d385aeb16ffcf8979d1341215175afc
NSLog0 <prachaya.su@gmail.com>
2018-12-16 09:44:21 +0700

    fix model, api

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 from flask import request
 from flask_restplus import Resource, Api, fields
 import hashlib
@@ -16,7 +16,7 @@ def create_app(test_config=None):
     app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
     api = Api(app, version='1.0', title='Sample API',
               description='A sample API')
-    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    # cors = CORS(app, resources={r"/*": {"origins": "*"}})
     app.config.from_mapping(
         SECRET_KEY='dev'
     )
@@ -30,7 +30,7 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    app.config['CORS_HEADERS'] = 'application/json'
+    # app.config['CORS_HEADERS'] = 'application/json'
 
     # ensure the instance folder exists
     try:
@@ -50,8 +50,8 @@ def create_app(test_config=None):
     parser.add_argument('Resist_short',  type=float,  help='Resistivity sensor reading at shallow radius of investigation', location='form')
     parser.add_argument('Density',      type=float,  help='Density sensor reading', location='form')
     parser.add_argument('Neutron',       type=float,  help='Neutron sensor reading', location='form')
-    parser.add_argument('FluidType',     help='Fluid containing in reservoir at specific depth from interpretation of 1 st well log', location='form')
-    parser.add_argument('Subblock',      help='Subsurface block categorized by geological structure', location='form')
+    # parser.add_argument('FluidType',     help='Fluid containing in reservoir at specific depth from interpretation of 1 st well log', location='form')
+    # parser.add_argument('Subblock',      help='Subsurface block categorized by geological structure', location='form')
     parser.add_argument('Thickness',     type=float,    help='Reservoir Thickness', location='form')
     parser.add_argument('Reservior',     help='Reservoir Name', location='form')
 
@@ -60,11 +60,34 @@ def create_app(test_config=None):
         @api.expect(parser)
         def post(self):
             args = parser.parse_args()
-            acc = mypd.predict(json.dumps(args))
             # import ipdb; ipdb.set_trace()
-            ans = 'OTHER'
-            if acc[0] == 1:
-                ans = 'NORMAL'
-            return { "meta": { "code": 200, "message": "success" }, "data": { 'RFT': ans } }
+            acc = mypd.predict(json.dumps(args))
+            is_nomal = 'OTHER'
+            fluid_type = 'not oil'
+
+            if acc[1] == 1:
+                is_nomal = 'NORMAL'
+            if acc[0] ==  1:
+                fluid_type = 'Oli'
+
+            return { "meta": { "code": 200, "message": "success" }, "data": {
+                'RFT': is_nomal, 'fluid_type': fluid_type,'mobility_score':
+                acc[2] } }
+
+
+
+    # parser = api.parser()
+    # parser.add_argument('sdfsdfs',        help='ID of RFT tests', location='form')
+    # @api.route('{}/predsssssssictions'.format(api_version))
+    # class Predicdfsdfstions(Resource):
+    #     @api.expect(parser)
+    #     def post(self):
+    #         args = parser.parse_args()
+    #         # import ipdb; ipdb.set_trace()
+    #         acc = mypd.predict(json.dumps(args))
+    #         ans = 'OTHER'
+    #         if acc[0] == 1:
+    #             ans = 'NORMAL'
+    #         return { "meta": { "code": 200, "message": "success" }, "data": { 'RFT': ans } }
 
     return app
--- app/models/ai.py
+++ app/models/ai.py
@@ -7,17 +7,18 @@ import json
 class Predictor:
     def predict(data):
         rpath = os.path.abspath(os.path.dirname(__file__))
-        ss = os.path.join(rpath, '../train_models/ss.pkl')
-        clf = os.path.join(rpath, '../train_models/clf.pkl')
+        ss = os.path.join(rpath, '../train_models/new/ss.pkl')
+        clf = os.path.join(rpath, '../train_models/new/clf.pkl')
+        fluid_clf = os.path.join(rpath, '../train_models/new/fluid_type_clf.pkl')
+        reg = os.path.join(rpath, '../train_models/new/reg.pkl')
 
         clf = pickle.load(open(clf, "rb"))
         ss = pickle.load(open(ss, "rb"))
+        reg = pickle.load(open(reg, 'rb'))
+        fluid_clf = pickle.load(open(fluid_clf, 'rb'))
 
         # pp = os.path.join(rpath, '../train_models/data/RFT/Test1.csv')
         # df_test1 = pd.read_csv(pp)
-
-        unique_fluid_types = ['O', 'W', 'G', 'O?']
-        unique_subblocks = ['F', 'N', 'P', 'C', 'S', 'K', 'R', 'Q', 'D', 'J', 'G', 'O', 'E', 'L', 'M', 'I', 'H', 'B']
 
         features = ['DepthMD',
                     'DepthTVDSS',
@@ -28,42 +29,20 @@ class Predictor:
                     'Resist_short',
                     'Density',
                     'Neutron',
-                    'FluidType',
-                    'Subblock',
-                    'Thickness',]
-
-        # sample = df_test1.iloc[0:1]
-        # print(sample)
+                    'Thickness']
 
         sample = pd.io.json.json_normalize(json.loads(data))
         sample = sample[features]
 
-        lst_fluid_type = []
-        lst_subblock = []
+        reg_sample = sample[['Resist_deep', 'Resist_medium', 'Resist_short', 'Density', 'Neutron', 'GR']]
 
-        train_fluid_type = np.zeros(len(unique_fluid_types))
-        train_subblock = np.zeros(len(unique_subblocks))
-        this_fluid_type = sample['FluidType'].values[0][0]
-        this_fluid_type_index = unique_fluid_types.index(this_fluid_type)
-        this_subblock = sample['Subblock'].values[0][0]
-        this_subblock_index = unique_subblocks.index(this_subblock)
-        train_fluid_type[this_fluid_type_index] = 1
-        train_subblock[this_subblock_index] = 1
-        lst_fluid_type.append(train_fluid_type)
-        lst_subblock.append(train_subblock)
+        sample = np.asarray(sample)
+        reg_sample = np.asarray(reg_sample)
+        x_test = ss.transform(sample)
 
-        lst_fluid_type = np.asarray(lst_fluid_type)
-        lst_subblock = np.asarray(lst_subblock)
-        df_fluid_type = pd.DataFrame(lst_fluid_type)
-        df_subblock = pd.DataFrame(lst_subblock)
+        # predict fluid type
+        fluid_type = fluid_clf.predict(x_test)[0] # 1: Oil, 0: Not Oil
+        is_normal = clf.predict(x_test)[0] #1: NORMAL, 0: OTHER
+        mobility_score = reg.predict(reg_sample)[0] # Mobility score
 
-        X_train1 = sample.reset_index(drop=True).drop(columns=['FluidType', 'Subblock'])
-        df_fluid_type = df_fluid_type.reset_index(drop=True)
-        df_subblock = df_subblock.reset_index(drop=True)
-        df_one_hot_encoded = pd.concat([X_train1, df_fluid_type, df_subblock], axis=1, ignore_index=True)
-
-        to_predict = ss.transform(df_one_hot_encoded)
-        pred = clf.predict(to_predict)
-        print(pred)
-
-        return pred
+        return (fluid_type, is_normal, mobility_score)
